ab14110b77dd5ccb12e5b988d57edaac80d2f705
YueHonghui <hongf.yue@hotmail.com>
2019-07-30 18:16:51 +0800

    add example

---
+++ example/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/gokits/cfg"
+	"github.com/gokits/cfg/source/file"
+	stdlogrus "github.com/gokits/stdlogger/logrus"
+	"github.com/sirupsen/logrus"
+	validator "gopkg.in/go-playground/validator.v9"
+)
+
+type Config struct {
+	Mode    string `validate:"oneof=file net"`
+	SnapLen int    `validate:"min=32,max=1024000"`
+}
+
+var (
+	filesource *file.File
+	cfgfile    string
+	meta       *cfg.ConfigMeta
+	gvalidator *validator.Validate
+)
+
+func (c *Config) PostDecode(oldptr interface{}) error {
+	var err error
+	if err = gvalidator.Struct(c); err != nil {
+		return errors.New(fmt.Sprintf("validate config failed: %+v", err))
+	}
+	return nil
+}
+
+func (c *Config) PostSwap(oldptr interface{}) {
+	logrus.Infof("config swapped: %+v", c)
+}
+
+func main() {
+	flag.StringVar(&cfgfile, "cfg", "./test.json", "file path of config file")
+	flag.Parse()
+
+	var err error
+
+	if filesource, err = file.NewFileSource(cfgfile, file.WithLogger(stdlogrus.FromGlobal())); err != nil {
+		logrus.Fatal(err)
+	}
+	meta = cfg.NewConfigMeta(Config{}, filesource, cfg.WithLogger(stdlogrus.FromGlobal()))
+	gvalidator = validator.New()
+	defer Fini()
+	go meta.Run()
+	if err = meta.WaitSynced(); err != nil {
+		filesource.Close()
+		logrus.WithError(err).Errorf("waitsync failed")
+		return
+	}
+	logrus.Infof("after waitsynced: %+v", Get())
+	go func() {
+		for {
+			time.Sleep(3 * time.Second)
+			logrus.Info(Get())
+		}
+	}()
+	time.Sleep(time.Hour)
+	return
+}
+
+func Fini() {
+	meta.Stop()
+	filesource.Close()
+}
+
+func Get() *Config {
+	return meta.Get().(*Config)
+}
--- go.mod
+++ go.mod
@@ -4,5 +4,11 @@ go 1.12
 
 require (
 	github.com/fsnotify/fsnotify v1.4.7
+	github.com/go-playground/locales v0.12.1 // indirect
+	github.com/go-playground/universal-translator v0.16.0 // indirect
+	github.com/gokits/gotools v0.2.0
 	github.com/gokits/stdlogger v0.2.1
+	github.com/leodido/go-urn v1.1.0 // indirect
+	github.com/sirupsen/logrus v1.4.2
+	gopkg.in/go-playground/validator.v9 v9.29.1
 )
